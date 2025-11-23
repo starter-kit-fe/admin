a753e1d5326177504c1299868883b4eef5ef288b
Your Name <you@example.com>
2025-11-23 14:19:59 +0800

    bump version to v25.1123.1419

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -4,14 +4,23 @@ import { NextConfig } from 'next';
 // OpenNext Cloudflare 仍依赖 Webpack 构建，显式关闭 Turbopack 以避免缺失 chunk。
 process.env.NEXT_SKIP_TURBOPACK = '1';
 
+const proxy = async () => {
+  return [
+    {
+      source: '/api/:path*',
+      destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
+    },
+  ];
+};
 const nextConfig: NextConfig = {};
 switch (process.env.NODE_ENV) {
   case 'production':
-    // nextConfig.output = 'export';
+    nextConfig.output = 'export';
     nextConfig.images = {};
     nextConfig.images.unoptimized = true;
     break;
   case 'development':
+    nextConfig.rewrites = proxy;
     break;
 }
 
